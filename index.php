acff7c3916cb1df5eb522899a83ba683e604e889
Colin Crampton <colin.crampton@iwanamaker.com>
2016-10-04 08:50:39 -0400

    Style

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 ?>
 
 <body>
-    <div id="top-bar"><div class="wrapper"><img class="logo" src="images/logo.png" /><div class="button apply">Apply</div><div class="button">Logs</div></div></div>
+    <div id="top-bar"><div class="wrapper"><img class="logo" src="images/logo.png" /><div class="button apply">Apply</div><div class="button">Logs</div><div class="button">News</div></div></div>
     <div class="wrapper">
-        <div class="rank"><table><tr><th>Realm:</th><td><?php echo $rank->realm_rank; ?></td></tr><tr><th>USA:</th><td><?php echo $rank->area_rank; ?></td></tr><tr><th>World:</th><td><?php echo $rank->world_rank; ?></td></tr></table></div>
-        <div class="next-raid"><h3>Next Raid:</h3><p>Emerald Nightmare on Tuesday, 10/4.</p></div>
-        <div class="logs"><h3>Recent Logs</h3>
+        <div id="home">
+          <div class="rank"><table><tr><th>Realm:</th><td><?php echo $rank->realm_rank; ?></td></tr><tr><th>USA:</th><td><?php echo $rank->area_rank; ?></td></tr><tr><th>World:</th><td><?php echo $rank->world_rank; ?></td></tr></table></div>
+          <div class="next-raid"><h3>Next Raid:</h3><p>Emerald Nightmare on Tuesday, 10/4.</p></div>
+        </div>
+        <div id="logs" style="display:none;"><h3>Recent Logs</h3>
         <?php
             foreach($parses as $p) {
                 $url = 'https://www.warcraftlogs.com/reports/' . $p->id;
