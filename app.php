a8cdc3f98ecf9a4cd6942db76e4dffe3a855bbc1
Colin Crampton <colin.crampton@iwanamaker.com>
2016-10-08 11:14:59 -0400

    Mailer Fix

--- app.php
+++ app.php
@@ -1,10 +1,10 @@
 <?php
-  mail('crcrampton@gmail.com', 'Big Things Application',
-	   "Character Name: " . $_POST['char'] .
+  mail('crcrampton@gmail.com', 'Big Things Application - ' . $_POST['cname'],
+	   "Character Name: " . $_POST['cname'] .
 	   "\nBattleTag: " . $_POST['btag'] .
 	   "\nEquipped ilvl: " . $_POST['ilvl'] .
 	   "\nSpec and Class: " . $_POST['spec'] .
 	   "\nArmory: " . $_POST['armory'] .
 	   "\n\nRaid Experience: " . $_POST['rexp'] .
-	   "\n\nAvailability: " . $_POST['avail']);
+	   "\n\nAvailability: " . $_POST['avail'], 'From: Big Things <no-reply@bigthin.gs>');
 ?>
